0098441f4638925bde874ef91111a0e7d5323bf7
Brian Lai <brianlai@Brians-Macbook.local>
2020-10-05 15:27:45 -0700

    add format select menu

--- color-palette-app/src/Palette.js
+++ color-palette-app/src/Palette.js
@@ -6,8 +6,9 @@ import './Palette.css';
 export default class Palette extends Component {
     constructor(props) {
         super(props);
-        this.state = { level: 500 };
+        this.state = { level: 500, format: 'hex' };
         this.changeLevel = this.changeLevel.bind(this);
+        this.changeFormat = this.changeFormat.bind(this);
     }
 
     changeLevel(level) {
@@ -15,15 +16,19 @@ export default class Palette extends Component {
         // console.log(level);
     }
 
+    changeFormat(val) {
+        this.setState({ format: val }); 
+    }
+
     render() {
         const {colors} = this.props.palette;
-        const {level} = this.state;
+        const {level, format} = this.state;
         const colorBoxes = colors[level].map(color => (
-            <ColorBox background={color.hex} name={color.name}/>
+            <ColorBox background={color[format]} name={color.name}/>
         ));
         return (
             <div className="Palette">
-                <NavBar level={level} changeLevel={this.changeLevel}/>
+                <NavBar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat}/>
                 <div className="Palette-colors">
                     {colorBoxes}
                 </div>

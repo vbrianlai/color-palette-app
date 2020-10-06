82dbbeb329f8b1a89ca04d554fb6ececd01208c4
Brian Lai <brianlai@Brians-Macbook.local>
2020-10-06 12:31:23 -0700

    add paletteList, miniColorBoxes, withStyle

--- color-palette-app/src/Palette.js
+++ color-palette-app/src/Palette.js
@@ -22,7 +22,6 @@ export default class Palette extends Component {
 
     render() {
         const {colors, paletteName, emoji} = this.props.palette;
-        console.log(paletteName)
         const {level, format} = this.state;
         const colorBoxes = colors[level].map(color => (
             <ColorBox background={color[format]} name={color.name} key={color.id}/>

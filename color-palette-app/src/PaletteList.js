81baeb83aee19d0a8b28fb5ea08cff03cbf06de3
Brian Lai <brianlai@Brians-Macbook.local>
2020-10-06 14:43:41 -0700

    add links to palettes

--- color-palette-app/src/PaletteList.js
+++ color-palette-app/src/PaletteList.js
@@ -34,6 +34,11 @@ const styles = {
 };
 
 class PaletteList extends Component {
+
+    goToPalette(id) {
+        this.props.history.push(`/palette/${id}`)
+    }
+
     render() {
         const {palettes, classes} = this.props;
         return (
@@ -43,8 +48,11 @@ class PaletteList extends Component {
                         <h1>React Colors</h1>
                     </nav>
                     <div className={classes.palettes}>
-                        {palettes.map(palette => (
-                            <MiniPalette {...palette} />
+                        {palettes.map(palette => (                           
+                            <MiniPalette 
+                                {...palette} 
+                                handleClick={() => this.goToPalette(palette.id)}
+                            />
                         ))}
                     </div>
                 </div>

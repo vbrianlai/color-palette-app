7724649208ab2b0ede52cbc62d139bfe866784d4
Brian Lai <vbrianlai@gmail.com>
2020-10-12 12:08:00 -0700

    fixed drawer button, disable react strict mode

--- color-palette-app/src/PaletteFormNav.js
+++ color-palette-app/src/PaletteFormNav.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react'
 import clsx from 'clsx';
 import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 // import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import Button from '@material-ui/core/Button';
+import DraggableColorList from './DraggableColorList';
 
 import {ChromePicker} from 'react-color';
 import {ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
+// import { arrayMove } from 'react-sortable-hoc';
 import arrayMove from 'array-move';
 import { Link } from 'react-router-dom';
 

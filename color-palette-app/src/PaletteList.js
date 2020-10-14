d657ad1df37ba4c8e0cad028456b328d783a9548
Brian Lai <vbrianlai@gmail.com>
2020-10-14 14:00:46 -0700

    add deletion confirmation dialog

--- color-palette-app/src/PaletteList.js
+++ color-palette-app/src/PaletteList.js
@@ -1,17 +1,52 @@
 import React, { Component } from 'react';
 import MiniPalette from './MiniPalette';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Avatar from '@material-ui/core/Avatar';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import CheckIcon from '@material-ui/icons/Check';
+import CloseIcon from '@material-ui/icons/Close';
 import {Link} from 'react-router-dom';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import styles from './styles/PaletteListStyles';
+import blue from '@material-ui/core/colors/blue';
+import red from '@material-ui/core/colors/red'
 import {withStyles} from '@material-ui/styles';
 
 class PaletteList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            openDeleteDialog : false,
+            deleteId: ''
+        }
+        this.openDialog = this.openDialog.bind(this);
+        this.closeDialog = this.closeDialog.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+
+    openDialog(id) {
+        this.setState({openDeleteDialog: true, deleteId: id});
+    }
+    
+    closeDialog() {
+        this.setState({openDeleteDialog: false, deleteId: ''});
+    }
 
     goToPalette(id) {
-        this.props.history.push(`/palette/${id}`)
+        this.props.history.push(`/palette/${id}`);
+    }
+    
+    handleDelete() {
+        this.props.deletePalette(this.state.deleteId);
+        this.closeDialog();
     }
 
     render() {
+        const {openDeleteDialog, deleteId} = this.state;
         const {palettes, classes, deletePalette} = this.props;
         return (
             <div className={classes.root}>
@@ -26,7 +61,8 @@ class PaletteList extends Component {
                                 <MiniPalette 
                                     {...palette} 
                                     handleClick={() => this.goToPalette(palette.id)}
-                                    handleDelete={deletePalette}
+                                    // handleDelete={deletePalette}
+                                    openDialog={this.openDialog}
                                     key={palette.id}
                                     id={palette.id}
                                 />
@@ -34,6 +70,27 @@ class PaletteList extends Component {
                         ))}
                     </TransitionGroup>
                 </div>
+                <Dialog open={openDeleteDialog} aria-labelledby='delete-dialog-title' onClose={this.closeDialog}>
+                    <DialogTitle id='delete-dialog-title'>Delete This Palette?</DialogTitle>
+                    <List>
+                        <ListItem button onClick={this.handleDelete}>
+                            <ListItemAvatar>
+                                <Avatar style={{backgroundColor: blue[100], color: blue[600]}}>
+                                    <CheckIcon/>
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary='Delete'/>
+                        </ListItem>
+                        <ListItem button onClick={this.closeDialog}>
+                            <ListItemAvatar>
+                                <Avatar style={{backgroundColor: red[100], color: red[600]}}>
+                                    <CloseIcon/>
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary='Cancel'/>
+                        </ListItem>
+                    </List>
+                </Dialog>
             </div>
         )
     }
